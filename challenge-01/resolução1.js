0a19366ad7cce34bf9b416a0e91045a3738b7d5c
vitor perez <vitorlperez1@gmail.com>
2020-04-06 16:02:28 -0400

    Exercicios resolvidos

--- challenge-01/resolução1.js
+++ challenge-01/resolução1.js
@@ -2,7 +2,7 @@
 var myvar = null;
 
 // Após declarada, atribua o valor 10 à variável `myvar`.
-myvar = 10
+myvar = 10;
 
 // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
 var soma = 15 + 8;
@@ -14,7 +14,7 @@ soma++;
 soma *= 3;
 
 // Qual é o valor da variável `soma` até aqui?
-72;
+console.log(soma);
 
 // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
 souninja = true;
@@ -26,12 +26,15 @@ var comida = ['arroz', 'feijão', 'ovo'];
 console.log(comida[1]);
 
 // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-
+console.log(soma === myvar);
 
 // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-
+console.log(myvar < soma);
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-
+function divisao(x,y){
+    return x/y;
+}
 
 // Invoque a função criada acima, passando os parâmetros 10 e 2.
+console.log(divisao(10,2))
